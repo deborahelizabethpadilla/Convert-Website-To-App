a7ca67b90a202641c228fd08f09cbe877de4288c
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-14 12:28:40 -0400

    First Commit: Add Files

---
+++ ConvertWebsiteApp/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  ConvertWebsiteApp
+//
+//  Created by Deborah on 3/14/17.
+//  Copyright © 2017 Deborah. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIWebViewDelegate {
+
+    @IBOutlet var webView: UIWebView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //Allow Website In View
+        
+        let url = NSURL(string: "http://jafty.com")
+        let request = URLRequest(url: url! as URL)
+        webView.loadRequest(request)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,1 @@
+#Convert-Website-To-App
