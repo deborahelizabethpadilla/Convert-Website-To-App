0eb14f8d1cd49d2776ed065063a80fc5eda124d1
Deborah Elizabeth Padilla <deborahelizabethpadilla@gmail.com>
2017-03-14 12:39:26 -0400

    Edits: ViewDidLoad

--- ConvertWebsiteApp/ViewController.swift
+++ ConvertWebsiteApp/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController, UIWebViewDelegate {
         //Allow Website In View
         
         let url = NSURL(string: "http://www.deborahelizabethpadilla.com")
+        
         let request = URLRequest(url: url! as URL)
         webView.loadRequest(request)
     }
